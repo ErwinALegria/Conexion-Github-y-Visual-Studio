836a3ed4cf7dd2d2bcdf170506c42f15cf7475fa
yerie <yerie@DESKTOP-31G9PIS>
2023-10-02 23:14:02 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conexion_Github_y_Visual_Studio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
